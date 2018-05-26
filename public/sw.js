ebda2aca78e5cb16ae28846291b149648fc81c81
MadaZZ <sparshranaazz@gmail.com>
2018-05-26 17:41:41 +0530

    Added functions to sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,12 @@
- 
+self.addEventListener('install', function (event) {
+    console.log('[serviceWorker] Installing service worker...', event);
+});
+
+self.addEventListener('activate', function (event) {
+    console.log('[serviceWorker] Activating service worker...', event);
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('[serviceWorker] fetch triggered...', event);
+    event.respondWith(fetch(event.request));
+});
